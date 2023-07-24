3018dfbaf71aaa3af37df85735e07a05731407bd
Ihor <igormochalov1881@gmail.com>
2023-07-24 16:27:23 +0300

    text script

---
+++ PlaylistScrapingBot/UIElements/Text.cs
@@ -0,0 +1,13 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+public sealed class Text
+{
+    public static void Draw(string text, Vector2 position, int size, Color color)
+    {
+        if (size < 1)
+            size = 1;
+
+        Raylib.DrawText(text, (int)position.X, (int)position.Y, size, color);
+    }
+}
