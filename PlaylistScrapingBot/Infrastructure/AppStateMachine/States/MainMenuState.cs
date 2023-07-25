af634e796c5a3d5356b3e430c50e07ad2425980d
Ihor <igormochalov1881@gmail.com>
2023-07-25 05:09:06 +0300

    app state machine states

---
+++ PlaylistScrapingBot/Infrastructure/AppStateMachine/States/MainMenuState.cs
@@ -0,0 +1,49 @@
+﻿using Raylib_cs;
+
+public sealed class MainMenuState : IState
+{
+    private readonly AppStateMachine _stateMachine;
+    private readonly IUIFactory _uiFactory;
+
+    private bool _startJorneyButtonWasPressedPastFrame;
+
+    public MainMenuState(AppStateMachine stateMachine, IUIFactory uiFactory)
+    {
+        _stateMachine = stateMachine;
+        _uiFactory = uiFactory;
+    }
+
+    public void Enter()
+    {
+    }
+
+    public void Update()
+    {
+        // Text
+        _uiFactory.CreateText("Playlist scraping bot welcomes you!", 150, Color.YELLOW, 30);
+
+        // Button
+        Button? startJorneyButton = _uiFactory.CreateButton("Start jorney", Color.RED, Color.BLACK, 400, 150, 75);
+        if (startJorneyButton is not null)
+        {
+            startJorneyButton.Update();
+
+            if (_startJorneyButtonWasPressedPastFrame && Raylib.IsMouseButtonUp(MouseButton.MOUSE_LEFT_BUTTON))
+                HandleStartJorneyButtonPressed();
+
+            if (startJorneyButton.IsPressed)
+                _startJorneyButtonWasPressedPastFrame = true;
+            else
+                _startJorneyButtonWasPressedPastFrame = false;
+        }
+    }
+
+    public void Exit()
+    {
+    }
+
+    private void HandleStartJorneyButtonPressed()
+    {
+        _stateMachine.Enter<ScrapingMenuState>();
+    }
+}
---
+++ PlaylistScrapingBot/Infrastructure/AppStateMachine/States/ScrapingMenuState.cs
@@ -0,0 +1,49 @@
+﻿using Raylib_cs;
+
+public class ScrapingMenuState : IState
+{
+    private readonly AppStateMachine _stateMachine;
+    private readonly IUIFactory _uiFactory;
+
+    private InputField? _inputField;
+    private Button? _checkIfURLValidButton;
+
+    public ScrapingMenuState(AppStateMachine stateMachine, IUIFactory uiFactory)
+    {
+        _stateMachine = stateMachine;
+        _uiFactory = uiFactory;
+    }
+
+    public void Enter()
+    {
+    }
+
+    public void Update()
+    {
+        // Text
+        _uiFactory.CreateText("Put your youtube playlist link into text filed below:", yOffset: 150, Color.MAGENTA);
+        
+        // Input field
+        _inputField = _uiFactory.CreateInputField(350, 450, 50, 20);
+        _inputField?.Update();
+
+        // Button
+        _checkIfURLValidButton = _uiFactory.CreateButton("Scrap the link", Color.RED, Color.BLACK, 500, 150, 75);
+        if (_checkIfURLValidButton is not null)
+        {
+            _checkIfURLValidButton.Update();
+
+            if (_checkIfURLValidButton.IsPressed)
+                CheckIfURLValid();
+        }
+    }
+
+    public void Exit()
+    {
+    }
+
+    private void CheckIfURLValid()
+    {
+        // check url with URLService
+    }
+}
