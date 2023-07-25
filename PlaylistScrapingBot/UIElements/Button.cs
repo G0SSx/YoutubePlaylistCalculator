a4c0702f31be39a5db61a46a471f661a39fa0f26
Ihor <igormochalov1881@gmail.com>
2023-07-25 05:07:58 +0300

    Button completed script

--- PlaylistScrapingBot/UIElements/Button.cs
+++ PlaylistScrapingBot/UIElements/Button.cs
@@ -3,7 +3,9 @@ using System.Numerics;
 
 public sealed class Button
 {
-    private readonly Rectangle _rect;
+    public bool IsPressed { get; private set; }
+    public Rectangle Rect { get; init; }
+
     private readonly Color _colorButton;
     private readonly Color _textColor;
     private readonly string _text;
@@ -12,30 +14,39 @@ public sealed class Button
 
     public Button(Rectangle rect, Color buttonColor, string text, Color textColor, int textSize = 20)
     {
-        _rect = rect;
+        Rect = rect;
         _colorButton = buttonColor;
         _text = text;
         _textColor = textColor;
         _textSize = textSize;
 
         _textPosition = new(
-            _rect.x + _rect.width / 2 - Raylib.MeasureText(_text, 20) / 2, 
-            _rect.y + _rect.height / 2);
+            Rect.x + Rect.width / 2 - Raylib.MeasureText(_text, 20) / 2,
+            Rect.y + Rect.height / 2);
     }
 
-    public void Update(bool isPressed)
+    public void Update()
     {
-        Action toDo = isPressed ? DrawPressed : Draw;
-        toDo.Invoke();
+        if (Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), Rect) &&
+            Raylib.IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON))
+        {
+            DrawRectPressed();
+            IsPressed = true;
+        }
+        else
+        {
+            DrawRect();
+            IsPressed = false;
+        }
 
         DrawText();
     }
 
-    private void Draw() => Raylib.DrawRectangleRec(_rect, _colorButton);
+    private void DrawRect() => Raylib.DrawRectangleRec(Rect, _colorButton);
 
-    private void DrawPressed() => 
-        Raylib.DrawRectangleRec(_rect, new(_colorButton.r, _colorButton.g, _colorButton.b, Convert.ToByte(200)));
+    private void DrawRectPressed() =>
+        Raylib.DrawRectangleRec(Rect, new Color(_colorButton.r, _colorButton.g, _colorButton.b, Convert.ToByte(200)));
 
-    private void DrawText() => 
+    private void DrawText() =>
         Raylib.DrawText(_text, (int)_textPosition.X, (int)_textPosition.Y, _textSize, _textColor);
 }
