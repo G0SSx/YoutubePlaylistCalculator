faca1e99aecd644d57da6429655e73b67692ece4
Ihor <igormochalov1881@gmail.com>
2023-07-25 05:08:46 +0300

    app state machine script

---
+++ PlaylistScrapingBot/Infrastructure/AppStateMachine/AppStateMachine.cs
@@ -0,0 +1,41 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+public sealed class AppStateMachine
+{
+    private readonly Dictionary<Type, IState> _states;
+
+    private IState? _state;
+
+    public AppStateMachine(IUIFactory uiFactory)
+    {
+        _states = new Dictionary<Type, IState>
+        {
+            [typeof(MainMenuState)] = new MainMenuState(this, uiFactory),
+            [typeof(ScrapingMenuState)] = new ScrapingMenuState(this, uiFactory),
+        };
+    }
+
+    public void Enter<TStateType>() where TStateType : IState
+    {
+        _state?.Exit();
+        _state = _states[typeof(TStateType)];
+        _state?.Enter();
+    }
+
+    public void Run()
+    {
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.DARKGRAY);
+
+            if (_state is null)
+                Console.WriteLine("Current state is null!");
+            else
+                _state.Update();
+
+            Raylib.EndDrawing();
+        }
+    }
+}
