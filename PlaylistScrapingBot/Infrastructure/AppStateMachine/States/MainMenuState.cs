10919730601002c0897ed595c95d36ac9e20f2fb
Ihor <igormochalov1881@gmail.com>
2023-07-26 07:01:42 +0300

    MainMenuState updated

--- PlaylistScrapingBot/Infrastructure/AppStateMachine/States/MainMenuState.cs
+++ PlaylistScrapingBot/Infrastructure/AppStateMachine/States/MainMenuState.cs
@@ -23,7 +23,7 @@ public sealed class MainMenuState : IState
         _uiFactory.CreateText("Playlist scraping bot welcomes you!", 150, Color.YELLOW, 30);
 
         // Button
-        Button? startJorneyButton = _uiFactory.CreateButton("Start jorney", Color.RED, Color.BLACK, 400, 150, 75);
+        Button? startJorneyButton = _uiFactory.CreateButton("Start jorney", Color.RED, Color.BLACK, 400, 300, 75);
         if (startJorneyButton is not null)
         {
             startJorneyButton.Update();
