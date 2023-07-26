05df4adeb90257e592c3c68688132092eea00e99
Ihor <igormochalov1881@gmail.com>
2023-07-26 06:36:57 +0300

    program class small changes

--- PlaylistScrapingBot/Infrastructure/Program.cs
+++ PlaylistScrapingBot/Infrastructure/Program.cs
@@ -1,8 +1,12 @@
-﻿IUIFactory uiFactory = new UIFactory();
+﻿// Services
+IUIFactory uiFactory = new UIFactory();
+
+// Logic elements
 Window window = uiFactory.CreateWindow(1000, 800);
 AppStateMachine appStateMachine = new(uiFactory);
 appStateMachine.Enter<MainMenuState>();
 
+// App logic
 window.Init();
 appStateMachine.Run();
 window.Close();
